82f9291befe925cc5b33fb0bf031975feaa783ad
cetecom <cetecom@LCT50413>
2019-05-08 23:24:43 -0300

    Inicio proyecto, Menu, clase, coneccion, index

---
+++ lib/constantes.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+session_start();
+define("URLBASE",'http://localhost/Chileanway/');
+define("DIRBASE",'C:/xampp/htdocs/Chileanway');
+/*
+ * Parametros DB
+ * **/
+define("DBNAME",'equiposfutfol');
+define("DBUSR",'duoc');
+define("DBPWD",'');
+define("DBHOST",'localhost');
+
---
+++ lib/menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<div class="list-group">
+    
+    <a class="list-group-item" data-remote="true" href="#sub_categoria_4" id="categoria_4" data-toggle="collapse" data-parent="#sub_categoria_4" style="padding-left: 25px;">
+          <span class="fa fa-shopping-cart fa-lg fa-fw"></span>
+          <span style="margin-left: 25px;">Menu</span>
+          <span class="menu-ico-collapse"><i class="fa fa-chevron-down"></i></span>
+    </a>
+
+        <div class="collapse list-group-submenu" id="sub_categoria_4">
+          
+          <a data-remote="true" href="#sub_categoria_2" class="list-group-item sub-item" data-parent="#sub_categoria_4" data-parent="#sub_categoria_2" style="padding-left: 78px;">
+                Mantenedor Equipos
+                <span class="menu-ico-collapse"><i class="fa fa-chevron-down"></i></span>                  
+          </a>
+            
+            <div class="collapse list-group-submenu" id="sub_categoria_2">
+                <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_2" style="padding-left: 78px;">Ingresar Equipos</a>
+                <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_2" style="padding-left: 78px;">Actualizar Equipo</a>          
+                <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_2" style="padding-left: 78px;">Eliminar Equipo</a>          
+            </div>  
+            
+          <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_4" style="padding-left: 78px;">Canasta de Cambios</a>
+          <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_4" style="padding-left: 78px;">Resumen de Equipos</a>          
+        </div>
+</div>
+
